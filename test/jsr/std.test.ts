e033b0250a4c932f11c7e146ec3ede357290f238
Ally Summers <ally.summers@outlook.com.au>
2024-10-06 09:14:55 +1100

    change jsr tests to local

--- test/jsr/std.test.ts
+++ test/jsr/std.test.ts
@@ -1,6 +1,6 @@
 import { assertEquals } from "jsr:@std/assert";
 
-import { decodeBase64, encodeBase64 } from "https://esm.sh/jsr/@std/encoding@1.0.0/base64";
+import { decodeBase64, encodeBase64 } from "http://localhost:8080/@std/encoding@1.0.0/base64";
 
 Deno.test("@std/encoding", async () => {
   assertEquals(encodeBase64("hello"), "aGVsbG8=");
